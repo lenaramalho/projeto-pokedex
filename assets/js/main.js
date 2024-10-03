c507c00792abd438d95d5b94ef20934a5edb57e4
Milena Ramalho <milena.ramalho03@gmail.com>
2024-10-03 15:39:32 -0300

    Add files via upload
    Subindo primeira versão do projeto, já com todos os pokemons responsivos

---
+++ assets/js/main.js
@@ -0,0 +1,24 @@
+const pokemonList = document.getElementById('pokemonList')
+const loadMoreButton = document.getElementById('load-more-button')
+
+function convertPokemonToLi(pokemon) {
+    return `
+    <li class="pokemon ${pokemon.type}">
+                <span class="number">#${pokemon.number}</span>
+                <span class="name">${pokemon.name}</span>
+
+                <div class="detail">
+                    <ol class="types">
+                            ${pokemon.types.map((type) => `<li class="type ${type}">${type}</li>`).join((''))}
+                    </ol>
+                    <img src="${pokemon.photo}" 
+                    alt="${pokemon.name}">
+                </div>
+            </li>`
+}
+
+pokeApi.getPokemons().then((pokemons = []) => {
+    const newHTML = pokemons.map(convertPokemonToLi).join('')
+    pokemonList.innerHTML = newHTML
+    })
+
